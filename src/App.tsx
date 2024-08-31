884708df748d5f8ac055953e686ae64b45ad5198
Francis Ciroi <francisciroi@hotmail.com>
2024-08-31 13:17:06 -0400

    Added back class

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container text-center">
       <img
         src="/speechify-logo.png"
         alt="Speechify Logo"
